cb1f54ab91f044968164a4e108a2d5645514e674
PigeonPlayGames <158285083+PigeonPlayGames@users.noreply.github.com>
2024-04-20 15:25:36 +0100

    Update kylemono.js

--- Molopony/kylemono.js
+++ Molopony/kylemono.js
@@ -33,9 +33,9 @@ function updatePlayerInfo() {
 // Function to get X position based on player position
 function getPositionX(position) {
     if (position < 5) {
-        return 570 - (position * 70);
+        return 570 - (position * 63);
     } else if (position >= 5 && position < 10) {
-        return 570 - (position * 50);
+        return 570 - (position * 68);
     } else if (position >= 10 && position < 20) {
         return 50;
     } else if (position >= 20 && position < 30) {
