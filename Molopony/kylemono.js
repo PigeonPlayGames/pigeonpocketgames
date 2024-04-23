ff69ecf1263c14b9113364277986d8c9febc92b2
PigeonPlayGames <158285083+PigeonPlayGames@users.noreply.github.com>
2024-04-23 16:27:40 +0100

    Update kylemono.js

--- Molopony/kylemono.js
+++ Molopony/kylemono.js
@@ -68,8 +68,9 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('purchaseCost').textContent = purchaseCost;
             
             const propertyImage = document.getElementById('propertyImage');
-            propertyImage.src = `Molopony/Images/property${propertyNumber}.jpg`;
+            propertyImage.src = `https://pigeonpocketgames.github.io/Molopony/Images/property${propertyNumber}.jpg`;
             propertyImage.alt = `Property ${propertyNumber}`;
+
 
             propertyDialog.classList.add('show-dialog');
         } else {
