3764cb77fa1749ea04ac842a0f8ccf2e45820cfa
PigeonPlayGames <158285083+PigeonPlayGames@users.noreply.github.com>
2024-05-05 16:17:34 +0100

    Update kylemono.js

--- Molopony/kylemono.js
+++ Molopony/kylemono.js
@@ -139,9 +139,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function getPositionX(position) {
         if (position < 5) {
-            return 570 - (position * 48);
+            return 570 - (position * 40);
         } else if (position >= 5 && position < 10) {
-            return 570 - (position * 50);
+            return 570 - (position * 40);
         } else if (position >= 10 && position < 20) {
             return 50;
         } else if (position >= 20 && position < 30) {
